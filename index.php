dc74e36e998174b63425d6005825eae1fbb88bfa
sliekasirdis79 <iglisbalys@gmail.com>
2016-01-06 16:35:32 +0200

    pusiau translate

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include "inc/init.php";
 
 
 
-$page->title = "Welcome to ". $set->site_name;
+$page->title = "Sveiki atvyke į ". $set->site_name;
 
 $presets->setActive("home"); // we highlith the home link
 
@@ -23,14 +23,14 @@ echo "
 <div class=\"container\">
 
 <div class=\"hero-unit\">
-    <h1>Welcome ".$user->filter->username." </h1>
+    <h1>Sveikas ".$user->filter->username." </h1>
     <p>
         This is a template for a simple website. It includes a user system with signup, login, password recovery and many more. It's a perfect stratup for your projects.
     </p>";
 if(!$user->islg()) {
     echo "<p>
-        <a class=\"btn btn-primary btn-large\" href=\"$set->url/register.php\">Sign Up</a>
-        <a class=\"btn btn-large\" href=\"$set->url/login.php\">Login</a>
+        <a class=\"btn btn-primary btn-large\" href=\"$set->url/register.php\">Registruotis</a>
+        <a class=\"btn btn-large\" href=\"$set->url/login.php\">Prisijungti</a>
     </p>";
 
 }
